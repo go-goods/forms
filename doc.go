f23d70b5111fff28956abc73e80e2474e622252c
jeff <leterip@gmail.com>
2012-04-01 17:08:37 -0400

    added some retarded docs

--- doc.go
+++ doc.go
@@ -1,2 +1,15 @@
-package forms
+//Package forms provides a set of types and interfaces for specifying form loading and validation.
+/*
+This is the extended description that takes a while.
 
+Foo bar baz
+
+	some code
+	goes
+	in this block
+	right here yay 
+	preformatting
+
+ok Finishing up
+*/
+package forms
